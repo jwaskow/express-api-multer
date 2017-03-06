49cb390c39e003bfef9501a15211c16875b475de
jwaskow <thewasko@gmail.com>
2017-03-06 14:18:11 -0500

    Fix filename and folder problem
    Previously files were just getting saved in the root directory of
    our bucker.  Solved this by adding today's date as a folder name and
    prepending our filename with that.  The next thing we did via
    crypto.randomBytes is create a random string to save our file as
    on AWS.  The last thing we did was promisify both s3.upload and
    crypto.randomBytes so both implement promises that are resolved or
    rejected based on whether or not the work.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 const mime = require('mime');
 const path = require('path');
-
+const crypto = require('crypto');
 
 let file = {
   path: process.argv[2],
@@ -17,19 +17,39 @@ let file = {
 
 let mimeType = mime.lookup(file.path);
 let ext = path.extname(file.path);
-
+let folder = (new Date()).toISOString().split('T')[0];
 let stream = fs.createReadStream(file.path);
 
-console.log("stream is ", stream);
+new Promise((resolve, reject)=>{
+  crypto.randomBytes(16, (error, buffer)=>{
+    if(error){
+      reject(error);
+    }
+    else {
+      console.log("buffer is ", buffer);
+      resolve(buffer.toString('hex'));
+    }
+  });
+})
+.then((filename)=> {
+  let params = {
+    ACL: 'public-read',
+    ContentType: mimeType,
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: `${folder}/${filename}${ext}`,
+    Body: stream
+  };
 
-let params = {
-  ACL: 'public-read',
-  ContentType: mimeType,
-  Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: `${file.title}${ext}`,
-  Body: stream
-};
-
-s3.upload(params, function (err, data){
-  console.log(err, data);
-});
+  return new Promise((resolve, reject)=>{
+    s3.upload(params, function (error, data){
+      if(error){
+        reject(error);
+      }
+      else {
+        resolve(data);
+      }
+    });
+  });
+})
+.then(console.log)
+.catch(console.error);
